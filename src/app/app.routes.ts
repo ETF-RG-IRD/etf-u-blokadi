00475888af544e9f62aee921951f86d627a7f426
ftlframe <igornikolicgit@gmail.com>
2025-01-30 16:20:24 +0100

    Demands finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { DemandsComponent } from './demands/demands.component';
 
 export const routes: Routes = [
-    {path: '', component: IndexComponent}
+    {path: '', component: DemandsComponent}
 ];

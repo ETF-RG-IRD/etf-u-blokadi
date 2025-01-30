92df00cdf9472ba414f997215b48320503cc4e2f
Mark Mozarovski <mark.mozh@gmail.com>
2025-01-30 19:47:24 +0100

    add faq section

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-faq',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
-export class FaqComponent {
 
+export class FaqComponent {
+    faqItems = [
+      { question: "Šta je plenum?", answer: "Na plenumu studenti glasaju o bitnim pitanja.", isOpen: false },
+      { question: "Kako mogu da pomognem?", answer: "Donacijama (link) i izlaženjem na proteste.", isOpen: false }
+  ];
+
+  toggleItem(index: number): void {
+      this.faqItems[index].isOpen = !this.faqItems[index].isOpen;
+  }
 }

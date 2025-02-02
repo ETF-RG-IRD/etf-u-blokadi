52e72b2ad04a9a9d5c66a686e4707002437c8571
ftlframe <igornikolicgit@gmail.com>
2025-02-03 00:16:15 +0100

    Typewriter header, background image css and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TypewriterComponent } from "./typewriter/typewriter.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, TypewriterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'etf-u-blokadi';
+
+  
 }

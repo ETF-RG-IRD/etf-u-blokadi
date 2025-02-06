ea419953edf3dd6e4046bdc3224dbfa50c655a4a
krupanjac <arsen.djurdjev@gmail.com>
2025-02-06 11:34:15 +0100

    Add loading component and implement loading state in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TypewriterComponent } from "./typewriter/typewriter.component";
-import { HttpClient } from '@angular/common/http';
+import { LoaderComponent } from './loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, TypewriterComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, TypewriterComponent, LoaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'etf-u-blokadi';
+  loading = true;
 
-  
+  constructor(private router: Router) {
+    // Initial load delay
+    window.addEventListener('load', () => {
+      const delay = Math.floor(Math.random() * (1500 - 500 + 1)) + 500;
+      setTimeout(() => {
+        this.loading = false;
+      }, delay);
+    });
+
+    // Listen to route events
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+      }
+      if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        const delay = Math.floor(Math.random() * (1500 - 500 + 1)) + 500;
+        setTimeout(() => {
+          this.loading = false;
+        }, delay);
+      }
+    });
+  }
 }

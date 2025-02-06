b613ab16d1a1b79d496b00b3c3d6844500a58980
ftlframe <igornikolicgit@gmail.com>
2025-02-06 19:18:54 +0100

    Extended loader

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from '../loader/loader.component'; // Ensure correct path
 
@@ -9,7 +9,7 @@ import { LoaderComponent } from '../loader/loader.component'; // Ensure correct 
   templateUrl: './media.component.html',
   styleUrls: ['./media.component.css']
 })
-export class MediaComponent implements OnInit {
+export class MediaComponent implements AfterViewInit {
   instagramProfiles = [
     { username: 'etfublokadi' },
     { username: 'grf.blokade' },
@@ -22,46 +22,45 @@ export class MediaComponent implements OnInit {
   currentImageIndex = 0;
   mediaLoading = true; // Flag to control media spinner overlay.
 
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.loadInstagramEmbed();
     this.startImageSlideshow();
-
-    // Hide the loader after a delay
-    setTimeout(() => {
-      this.mediaLoading = false;
-    }, 500);
   }
 
   loadInstagramEmbed() {
     if ((window as any).instgrm) {
-      // If Instagram's script is already loaded, process embeds immediately
       (window as any).instgrm.Embeds.process();
+      this.ngOnLoad();
     } else {
-      // Load Instagram's embed script dynamically
       const script = this.renderer.createElement('script');
       script.src = 'https://www.instagram.com/embed.js';
       script.async = true;
       script.defer = true;
       script.onload = () => {
-        // Process embeds after the script is loaded
         (window as any).instgrm.Embeds.process();
+        this.ngOnLoad();
       };
       this.renderer.appendChild(document.body, script);
     }
   }
 
+  // Custom onLoad method for media component.
+  ngOnLoad() {
+    setTimeout(() => {
+      this.mediaLoading = false;
+    }, 1000);
+  }
+
   startImageSlideshow() {
     setInterval(() => {
       const currentImage = this.el.nativeElement.querySelector('.image-slider img');
-      if (currentImage) {
-        currentImage.classList.add('flash');
-        setTimeout(() => {
-          this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-          currentImage.classList.remove('flash');
-        }, 500);
-      }
+      currentImage.classList.add('flash');
+      setTimeout(() => {
+        this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+        currentImage.classList.remove('flash');
+      }, 500);
     }, 3000);
   }
 }

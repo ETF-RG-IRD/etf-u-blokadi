f216e071d28e137301c66e1a499e50b8cf470962
ftlframe <ftlframe@mailfence.com>
2025-02-19 01:44:27 +0100

    Working on websocket for telegram integration

--- src/app/features/announcements/announcements.component.ts
+++ src/app/features/announcements/announcements.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WipComponent } from '../../wip/wip.component';
+import { WebSocketService } from '../../services/web-socket.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-announcements',
-  imports: [WipComponent],
+  imports: [CommonModule],
   templateUrl: './announcements.component.html',
   styleUrl: './announcements.component.css'
 })
-export class AnnouncementsComponent {
+export class AnnouncementsComponent implements OnInit, OnDestroy {
+  private messageSubscription: Subscription | undefined;
+  public messages: any[] = [];
 
+  constructor(private webSocketService: WebSocketService) { }
+
+  ngOnInit(): void {
+    this.messageSubscription = this.webSocketService.get_messages().subscribe({
+      next: (message) => {
+        let data = JSON.parse(message).data
+        data = JSON.parse(data)
+
+
+        this.messages.push(data);
+
+      },
+      error: (error) => {
+        console.error('Error receiving message:', error);
+      },
+      complete: () => {
+        console.log('WebSocket connection closed');
+      },
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.messageSubscription) {
+      this.messageSubscription.unsubscribe();
+    }
+
+    this.webSocketService.close_connection();
+  }
 }

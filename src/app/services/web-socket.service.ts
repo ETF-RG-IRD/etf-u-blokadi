095cb4c98ef64f4dc2bd742ece913ce1ba31eeb9
ftlframe <ftlframe@mailfence.com>
2025-03-04 17:26:40 +0100

    Fix

--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { environment } from '../../environments/environment.development.js';
 
 
 @Injectable({
